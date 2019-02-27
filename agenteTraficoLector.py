41aabb23082b2751064efc93b4a0c9b421e51fa1
Jose Manuel Mendez <jmanuel34@gmail.com>
2019-02-27 20:40:16 +0100

    Añadido el fichero para que lea del directorio

--- agenteTraficoLector.py
+++ agenteTraficoLector.py
@@ -5,7 +5,7 @@ patron1 = '<.*?>'
 patronEspacio = '\s+'
 pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?alt=(.*?)/(.*?)/(.*?)/.*?>M-40<.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
 
-p = '/Users/jmanuel/Desktop/Ai/trafico/traficoAgente.html'
+p = './trafico.html'
 with open(p, 'r', encoding='utf-8') as file:
     pagina = file.read()
     elements = re.findall(pttrn, pagina, re.DOTALL | re.MULTILINE | re.IGNORECASE)
@@ -14,16 +14,17 @@ with open(p, 'r', encoding='utf-8') as file:
         [line[0], line[1], line[2], line[3], line[4], re.sub(patron1, ' ', line[5])]
         for line in elements
     ]
+ #   print (elementos)
 
     elementos1 = [
-        [line[0], line[1], line[3], line[4], re.sub(patronEspacio, ' ', line[5])]
+        [line[0], line[1], line[2], line[3], line[4], re.sub(patronEspacio, ' ', line[5])]
         for line in elementos
     ]
-    #    print (elementos1)
+    print (elementos1)
     for element in elementos1:
         pprint(element[0])
         pprint(element[1])
         pprint(element[2])
         pprint(element[3])
         pprint(element[4])
- #       pprint(element[5])
+        pprint(element[5])
