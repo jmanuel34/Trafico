6dc47959ed7613425e1996c2c61d0767b2f36ffd
Jose Manuel Mendez <jmanuel34@gmail.com>
2019-02-28 14:45:22 +0100

    Cambios variados para fijar las versiones

--- agente.py
+++ agente.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python3.4
+
 import re
 import urllib.request
 import time
 from pprint import pprint
 
-p = '/Users/jmanuel/Desktop/Ai/trafico/trafico1.txt'
-request = urllib.request.Request(
-    "http://infocar.dgt.es/etraffic/Incidencias?ca=13&provIci=28&caracter=acontecimiento&accion_consultar=Consultar&IncidenciasRETENCION=IncidenciasRETENCION&IncidenciasPUERTOS=IncidenciasPUERTOS&IncidenciasMETEOROLOGICA=IncidenciasMETEOROLOGICA&IncidenciasEVENTOS=IncidenciasEVENTOS&IncidenciasOTROS=IncidenciasOTROS&IncidenciasRESTRICCIONES=IncidenciasRESTRICCIONES&ordenacion=fechahora_ini-DESC")
-result = urllib.request.urlopen(request)
+p = "./traficoResults.txt"
+url="http://infocar.dgt.es/etraffic/Incidencias?ca=13&provIci=28&caracter=acontecimiento&accion_consultar=Consultar&IncidenciasRETENCION=IncidenciasRETENCION&IncidenciasPUERTOS=IncidenciasPUERTOS&IncidenciasMETEOROLOGICA=IncidenciasMETEOROLOGICA&IncidenciasEVENTOS=IncidenciasEVENTOS&IncidenciasOTROS=IncidenciasOTROS&IncidenciasRESTRICCIONES=IncidenciasRESTRICCIONES&ordenacion=fechahora_ini-DESC"
+result = urllib.request.urlopen(url)
 pagina = (result.read().decode('utf8'))
 patron1 = '<.*?>'
 patronEspacio = '\s+'
 # pprint (pagina)
-pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?alt=(.*?)/(.*?)/(.*?)/.*?>M-40<.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
+#  Funcionando en Jupyter
+# pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?alt=(.*?)/(.*?)/(.*?)/.*?>M-111<.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
+
+pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?img src=.*?alt=(.*?)/(.*?)/(.*?)/.*?>M-40<.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
 with open(p, 'a', encoding='utf-8') as file:
+    print (pagina)
     elements = re.findall(pttrn, pagina, re.DOTALL | re.MULTILINE | re.IGNORECASE)
-    # pprint (elements)
+    pprint (elements)
     elementos = [
         [line[0], line[1], line[2], line[3], line[4], re.sub(patron1, ' ', line[5])]
         for line in elements
@@ -26,10 +31,15 @@ with open(p, 'a', encoding='utf-8') as file:
     ]
     print(elementos1)
     for element in elementos1:
-
         pprint(element[0])
         pprint(element[1])
         pprint(element[2])
         pprint(element[3])
         pprint(element[4])
         pprint(element[5])
+        file.write(element[0])
+        file.write(element[1])
+        file.write(element[2])
+        file.write(element[3])
+        file.write(element[4])
+        file.write(element[5])
--- agenteTraficoLector.py
+++ agenteTraficoLector.py
@@ -3,7 +3,7 @@ import re
 from pprint import pprint
 patron1 = '<.*?>'
 patronEspacio = '\s+'
-pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?alt=(.*?)/(.*?)/(.*?)/.*?>M-40<.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
+pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?alt=(.*?)/(.*?)/.*?>M-40<.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
 
 p = './trafico.html'
 with open(p, 'r', encoding='utf-8') as file:
@@ -11,20 +11,20 @@ with open(p, 'r', encoding='utf-8') as file:
     elements = re.findall(pttrn, pagina, re.DOTALL | re.MULTILINE | re.IGNORECASE)
 
     elementos = [
-        [line[0], line[1], line[2], line[3], line[4], re.sub(patron1, ' ', line[5])]
+        [line[0], line[1], line[2], line[3], re.sub(patron1, ' ', line[4])]
         for line in elements
     ]
  #   print (elementos)
 
     elementos1 = [
-        [line[0], line[1], line[2], line[3], line[4], re.sub(patronEspacio, ' ', line[5])]
+        [line[0], line[1], line[2], line[3], re.sub(patronEspacio, ' ', line[4])]
         for line in elementos
     ]
-    print (elementos1)
+#    print (elementos1)
     for element in elementos1:
         pprint(element[0])
         pprint(element[1])
         pprint(element[2])
         pprint(element[3])
         pprint(element[4])
-        pprint(element[5])
+ #       pprint(element[5])
