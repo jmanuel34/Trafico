9c36b4da174a1db4e4ecd3401d58a410eaa1cbb7
Jose Manuel Mendez <jmanuel34@gmail.com>
2019-02-28 17:54:28 +0100

    Incidencias M40 por fichero agente2.py

--- agente2.py
+++ agente2.py
@@ -6,11 +6,18 @@ p='/Users/jmanuel/Desktop/Ai/trafico/trafico1.txt'
 request= urllib.request.Request("http://infocar.dgt.es/etraffic/Incidencias?caracter=acontecimiento&orden=fechahora_ini%20DESC&IncidenciasOTROS=IncidenciasOTROS&IncidenciasEVENTOS=IncidenciasEVENTOS&IncidenciasRETENCION=IncidenciasRETENCION&IncidenciasPUERTOS=IncidenciasPUERTOS&IncidenciasMETEOROLOGICA=IncidenciasMETEOROLOGICA&IncidenciasRESTRICCIONES=IncidenciasRESTRICCIONES")
 result = urllib.request.urlopen(request)
 pagina= (result.read().decode('utf8'))
-#pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)*?ACCIDENTE.*?</tr>'
-#pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?.*?<b>(.*?)</b>.*?nombreIncidencia.*?ACCIDENTE.*?'
-#pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d).*?(\d\d/\d\d/\d\d\d\d).*?(Nivel.*?)/.*?/.*?<b>(.*?)</b>.*?p2TablaIncidencias.*?;">(.*?)</a>.*?nombreIncidencia.*?</tr>'
-pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d)</span.*?(\d\d/\d\d/\d\d\d\d).*?alt=(.*?)/(.*?)/(.*?)/.*?nombreIncidencia.*?span style="margin-top:10px; float:left; clear:both">(.*?)</tr>'
-
+pttrn = '<tr.*?p1TablaIncidencias.*?(\d\d:\d\d).*?(\d\d/\d\d/\d\d\d\d).*?(Nivel.*?)/.*?/.*?<b>(.*?)</b>.*?p2TablaIncidencias.*?;">(.*?)</a>.*?<b>(.*?)</b>.*?nombreIncidencia.*?clear:both">(.*?)</tr>'
+patron1 = '<.*?>'
+patron2= '\s+'
 elements = re.findall(pttrn, pagina, re.DOTALL | re.MULTILINE)
-pprint(elements)
-pprint(pagina)
+elementos = [
+            [line[0], line[1], line[2], line[3], line[4], line[5], re.sub(patron1, ' ', line[6])]
+            for line in elements
+    ]
+elementos1 = [
+            [line[0], line[1], line[2], line[3], line[4], line[5], re.sub(patron2, ' ', line[6])]
+            for line in elementos
+    ]
+for element in elementos1:
+    if(element[5] == "M-40"):
+        pprint(element)
